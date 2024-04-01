1913092f6082d96f45abf7b42fc56bc9ad66b13b
ahmetyesil <ahmet.koyuyesil@gmail.com>
2024-04-01 11:43:32 +0300

    ...

--- src/Core/UI/Common/Container.tsx
+++ src/Core/UI/Common/Container.tsx
@@ -1,12 +1,4 @@
 import * as React from "react";
-import { Header, Footer } from "@/Core/index";
-import { useRouter } from "next/router";
-import { useDispatch, useSelector } from "react-redux";
-import { RootState } from "@/Redux/reducers";
-import { useEffect } from "react";
-import { useMutateApi } from "@/Hooks/index";
-import { loginRedux, logoutRedux } from "@/Redux/actions";
-import DashboardHeader from "@/Composite/DashboardHeader";
 
 export type IProps = {
   children: React.ReactNode | JSX.Element | null | boolean;
